e54aaa5e819a561d484c971e7dd2faff7cbc8fcf
KimJung Woo <SeanKim@KimJungiMacBook.stu-wireless.wesleyan.edu>
2017-04-26 23:42:42 -0400

    More to go

--- public/js/home.js
+++ public/js/home.js
@@ -1,104 +1,133 @@
-// Wesleyan Courses Ajax Call
-var courseInfos;
-
-console.log("here");
-
-var noNameChanger = function(lst) {
-    if (lst === null)
-        return ["Not Available"];
-    lst = lst.filter((ob) => {
-        return ob["instructors"][0]["first_name"] !== "** ERROR:  Name";
-    }).map((ob,i,arr) => {
-        return ob["instructors"][0]["first_name"] + " " + ob["instructors"][0]["last_name"];
-    });
-    return (lst.length === 0) ? ["Not Available"] : lst;
-};
-
-function isEquivalent(a, b) {
-    // Create arrays of property names
-    var aProps = Object.getOwnPropertyNames(a);
-    var bProps = Object.getOwnPropertyNames(b);
-
-    // If number of properties is different,
-    // objects are not equivalent
-    if (aProps.length != bProps.length) {
-        return false;
-    }
-
-    for (var i = 0; i < aProps.length; i++) {
-        var propName = aProps[i];
-
-        // If values of same property are not equal,
-        // objects are not equivalent
-        if (a[propName] !== b[propName]) {
-            return false;
-            // if (propName !== "section")
-            //     return false;
-            // else {
-            //     a[propName] = "Same Class Different Sections";
-            //     b[propName] = "Same Class Different Sections";
-            // }
-        }
-    }
-
-    // If we made it this far, objects
-    // are considered equivalent
-    return true;
-};
-
-// var filterInfos = function(lst) {
-//     lst.forEach((obj,i,arr) => {
-//         arr.forEach((obj1,i1,arr1) => {
-//             if (i !== i1) {
-//                 if (isEquivalent(obj,obj1)) {
-//                     courseInfos.add()
-//                 }
+// // Wesleyan Courses Ajax Call
+// var courseInfos;
+//
+// console.log("here");
+//
+// var noNameChanger = function(lst) {
+//     if (lst === null)
+//         return ["Not Available"];
+//     lst = lst.filter((ob) => {
+//         return ob["instructors"][0]["first_name"] !== "** ERROR:  Name";
+//     }).map((ob,i,arr) => {
+//         return ob["instructors"][0]["first_name"] + " " + ob["instructors"][0]["last_name"];
+//     });
+//     return (lst.length === 0) ? ["Not Available"] : lst;
+// };
+//
+// function isEquivalent(a, b) {
+//     // Create arrays of property names
+//     var aProps = Object.getOwnPropertyNames(a);
+//     var bProps = Object.getOwnPropertyNames(b);
+//
+//     // If number of properties is different,
+//     // objects are not equivalent
+//     if (aProps.length != bProps.length) {
+//         return false;
+//     }
+//
+//     for (var i = 0; i < aProps.length; i++) {
+//         var propName = aProps[i];
+//
+//         // If values of same property are not equal,
+//         // objects are not equivalent
+//         if (a[propName] !== b[propName]) {
+//             return false;
+//             // if (propName !== "section")
+//             //     return false;
+//             // else {
+//             //     a[propName] = "Same Class Different Sections";
+//             //     b[propName] = "Same Class Different Sections";
+//             // }
+//         }
+//     }
+//
+//     // If we made it this far, objects
+//     // are considered equivalent
+//     return true;
+// };
+//
+// // var filterInfos = function(lst) {
+// //     lst.forEach((obj,i,arr) => {
+// //         arr.forEach((obj1,i1,arr1) => {
+// //             if (i !== i1) {
+// //                 if (isEquivalent(obj,obj1)) {
+// //                     courseInfos.add()
+// //                 }
+// //             }
+// //         })
+// //     })
+// // }
+// $('button[type="submit"]').on('click',function(e) {
+//     e.preventDefault();
+//     var searchQuery = $('input[type="text"]')[0].value.toLowerCase();
+//     console.log(typeof searchQuery);
+//     $('input[type="text"]')[0].value = '';
+//     $.ajax({
+//         url: `http://webapps-test.wesleyan.edu/wapi/v1/public/academic/courses/${new Date().getFullYear()}`
+//     }).done(res => {
+//         console.log(res);
+//         res = res["academic"]["courses"].map((course,index,arr) => {
+//             return {
+//                 subject: course.subject,
+//                 catalog_number: course.catalog_number,
+//                 description: course.description,
+//                 section: course.section,
+//                 instructors: noNameChanger(course["meetings"]).join(", ")
 //             }
+//         });
+//         console.log(res);
+//         console.log(typeof res[0].subject);
+//         courseInfos = res.filter((course) => {
+//             return course.subject.toLowerCase().search(searchQuery) !== -1 || course.catalog_number.toLowerCase().search(searchQuery) !== -1 || course.description.toLowerCase().search(searchQuery) !== -1 || course.section.toLowerCase().search(searchQuery) !== -1 || course.instructors.toLowerCase().search(searchQuery) !== -1 || (course.subject.toLowerCase()+course.catalog_number.toLowerCase()).search(searchQuery) !== -1 || (course.subject.toLowerCase()+course.catalog_number.toLowerCase().slice(1)).search(searchQuery) !== -1;
+//         });
+//         console.log(courseInfos);
+//
+//
+//         $('.all_courses').html(courseInfos.map((course,index,arr) => {
+//             return `
+//             <li class="list-group-item">
+// 				<a href="#">${course.subject + course.catalog_number}: ${course.description}
+// 				<br>
+// 				Section: ${course.section}
+// 				<br>
+// 				Instructors: ${course.instructors}</a>
+// 			</li>
+//     `
+//         }));
+//     });
+// });
+//
+//
+//
+//
+//
+//
+
+// $('button[type="submit"]').on('click',function(e) {
+//     e.preventDefault();
+//     var searchQuery = $('input[type="text"]')[0].value.toLowerCase();
+//     console.log(typeof searchQuery);
+//     $('.all_courses').html(`<h1 style="position: fixed; font-color: white; left:30%; top: 50%; translate:(-30%,-50%); font-size: 6vw">SEARCHING...</h1>`);
+//     if (searchQuery !== '') {
+//         $('input[type="text"]')[0].value = '';
+//         $.ajax({
+//             url : 'api/some_courses',
+//             data: {
+//                 urlExtension: searchQuery
+//             }
+//         }).then(courseInfos => {
+//             console.log(courseInfos);
+//             $('.all_courses').html(courseInfos.map((course, index, arr) => {
+//                 return `
+//                         <li class="list-group-item">
+//                             <a href="#">${course.subject + course.catalog_number}: ${course.description}
+//                             <br>
+//                             Section: ${course.section}
+//                             <br>
+//                             Instructors: ${course.instructors}</a>
+//                         </li>
+// `
+//             }));
 //         })
-//     })
-// }
-$('button[type="submit"]').on('click',function(e) {
-    e.preventDefault();
-    var searchQuery = $('input[type="text"]')[0].value.toLowerCase();
-    console.log(typeof searchQuery);
-    $('input[type="text"]')[0].value = '';
-    $.ajax({
-        url: `http://webapps-test.wesleyan.edu/wapi/v1/public/academic/courses/${new Date().getFullYear()}`
-    }).done(res => {
-        console.log(res);
-        res = res["academic"]["courses"].map((course,index,arr) => {
-            return {
-                subject: course.subject,
-                catalog_number: course.catalog_number,
-                description: course.description,
-                section: course.section,
-                instructors: noNameChanger(course["meetings"]).join(", ")
-            }
-        });
-        console.log(res);
-        console.log(typeof res[0].subject);
-        courseInfos = res.filter((course) => {
-            return course.subject.toLowerCase().search(searchQuery) !== -1 || course.catalog_number.toLowerCase().search(searchQuery) !== -1 || course.description.toLowerCase().search(searchQuery) !== -1 || course.section.toLowerCase().search(searchQuery) !== -1 || course.instructors.toLowerCase().search(searchQuery) !== -1 || (course.subject.toLowerCase()+course.catalog_number.toLowerCase()).search(searchQuery) !== -1 || (course.subject.toLowerCase()+course.catalog_number.toLowerCase().slice(1)).search(searchQuery) !== -1;
-        });
-        console.log(courseInfos);
-
-
-        $('.all_courses').html(courseInfos.map((course,index,arr) => {
-            return `
-            <li class="list-group-item">
-				<a href="#">${course.subject + course.catalog_number}: ${course.description}
-				<br>
-				Section: ${course.section}
-				<br>
-				Instructors: ${course.instructors}</a>
-			</li>
-    `
-        }));
-    });
-});
-
-
-
-
-
-
+//     }
+// });
--- server.js
+++ server.js
@@ -2,21 +2,75 @@ const bodyParser = require('body-parser');
 const express = require('express');
 // const {mongoose} = require('./db/mongoose');
 const methodOverride = require('method-override');
+var request = require('request-promise');
+
 var courseInfos;
+var courseRender;
+
+// Helper Functions
+var noNameChanger = function(lst) {
+    if (lst === null)
+        return ["TBA"];
+    lst = lst.map((ob) => {
+        return (ob["instructors"][0]["first_name"] === "** ERROR:  Name") ? "STAFF" : ob["instructors"][0]["first_name"] + " " + ob["instructors"][0]["last_name"];
+    });
+
+    var mySet = new Set();
+    lst.forEach((name) => {
+        mySet.add(name);
+    });
+    let newList = Array.from(mySet);
+    return (newList.length === 0 || newList[0].length === 3) ? ["STAFF"] : newList;
+};
+
+
+
+const option = {
+    uri: `http://webapps-test.wesleyan.edu/wapi/v1/public/academic/courses/${new Date().getFullYear()}`
+};
+
+request(option).then(res => {
+    // console.dir(JSON.parse(res),{depth:null});
+    res = JSON.parse(res);
+    res = res["academic"]["courses"].map((course,index,arr) => {
+        return {
+            subject: course.subject,
+            catalog_number: course.catalog_number,
+            description: course.description,
+            section: course.section,
+            instructors: noNameChanger(course["meetings"]).join(", ")
+        }
+    });
+    // console.log(res);
+    // console.log(typeof res[0].subject);
+    // courseRender = res.filter((course) => {
+    //     return course.subject.toLowerCase().search(searchQuery) !== -1 || course.catalog_number.toLowerCase().search(searchQuery) !== -1 || course.description.toLowerCase().search(searchQuery) !== -1 || course.section.toLowerCase().search(searchQuery) !== -1 || course.instructors.toLowerCase().search(searchQuery) !== -1 || (course.subject.toLowerCase()+course.catalog_number.toLowerCase()).search(searchQuery) !== -1 || (course.subject.toLowerCase()+course.catalog_number.toLowerCase().slice(1)).search(searchQuery) !== -1;
+    // });
+
+    // courseRender = courseRender.sort((a,b) => {
+    //     // console.log(a.subject,b.subject);
+    //     return a.subject.toLowerCase() > b.subject.toLowerCase()
+    // });
+
+    courseRender = res;
+
+    // console.dir(courseRender,{depth:null});
+});
+
 
 var app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
 
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
-app.use('/js', express.static(__dirname + '/node_modules'))
+app.use('/js', express.static(__dirname + '/node_modules'));
 
 app.use(express.static(__dirname));
 
@@ -27,16 +81,23 @@ app.listen(port, () => {console.log(`Server is running at ${port}`)});
 
 
 app.get('/',(req,res) => {
-    res.sendFile(__dirname + '/views/home.html');
+    res.render('home.ejs',{});
 });
 
-app.get('/api/courses',(req,res) => {
-    app.get('/Keyword?='+req.query.urlExtension,(request,result) => {
-        res.sendFile(__dirname + '/views/home.html');
-    });
-});
-
-
-app.get('/catalog', (req,res) => {
+app.get('/catalog',(req,res) => {
     res.render('catalog',{});
+})
+
+app.get('/api/all_courses',(req,res) => {
+    res.json(courseRender);
+});
+
+
+app.get('/search', (req,res) => {
+        var searchQuery = req.body.toLowerCase();
+        var result = courseRender.filter((course) => {
+            return course.subject.toLowerCase().search(searchQuery) !== -1 || course.catalog_number.toLowerCase().search(searchQuery) !== -1 || course.description.toLowerCase().search(searchQuery) !== -1 || course.section.toLowerCase().search(searchQuery) !== -1 || course.instructors.toLowerCase().search(searchQuery) !== -1 || (course.subject.toLowerCase()+course.catalog_number.toLowerCase()).search(searchQuery) !== -1 || (course.subject.toLowerCase()+course.catalog_number.toLowerCase().slice(1)).search(searchQuery) !== -1;
+        });
+        console.log(res);
+        res.render('home',{courseInfos: res});
 });
